0c145361e8714215b96758525b55e62bfb2baf1c
Preethi yadav <98330687+kalindalapreethiyadav@users.noreply.github.com>
2023-01-26 23:20:57 +0530

    commit & push

--- Pod1.sh
+++ Pod1.sh
@@ -6,3 +6,16 @@ spec:
   containers:
     - name: nginx
       image: nginx:1.14.2
+
+---
+apiVersion: v1
+kind: Pod
+metadata:
+  name: mutli-pod
+spec:
+  containers:
+    - name: nginx
+      image: nginx:1.14.2
+    - name: mongodb
+      image: mongod
+     
